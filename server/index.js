edc69eb23e280f2b549a295591ba7435025c6694
Umesh-malik-byjus <88026680+Umesh-malik-byjus@users.noreply.github.com>
2022-11-13 20:15:47 +0530

    init

--- server/index.js
+++ server/index.js
@@ -3,13 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs/promises')
-(async () => {
+
+const attachData = async () => {
     const privateKey = await fs.readFile('./keys/server.key');
     app.set('privateKey', privateKey);
     const publicKey = await fs.readFile('./keys/server.key.pub');
     app.set('publicKey', publicKey);
     app.set('passphrase', 'example');
-})()
+}
+attachData();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({limit: '10mb'}))
